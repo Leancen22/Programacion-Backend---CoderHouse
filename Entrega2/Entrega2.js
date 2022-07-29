63b6d98518d716ed683aa55218fa8a42dc58338d
Leandro Mesa <leandromesaprofesional@gmail.com>
2022-07-29 14:17:20 -0300

    Actualizacion entrega2

--- Entrega2/Entrega2.js
+++ Entrega2/Entrega2.js
@@ -1,70 +1,115 @@
-const fs = require('fs/promises')
+const fs = require('fs')
 
-const text = './productos.txt'
+class Contenedor {
 
-async function cargar() {
-    await fs.readFile(text, 'utf-8').then( contenido => {
-        if (contenido == '') {
-            fs.writeFile(text, '[]')
+    //Constructor de la clase contenedor
+    constructor (archivo) {
+        this.archivo = archivo
+    }
+
+    /* 
+        save (Object) : Number
+        
+        Recibe un objeto y retorna el id con el que se inserta al archivo
+    */
+    async save (obj) {
+        try {
+            const objetos = await this.getAll()
+
+            let newId
+            if (objetos.length == 0) {
+                newId = 1
+            } else {
+                newId = objetos[objetos.length - 1].id + 1
+            }
+
+            const newObj = {id: newId, ...obj}
+            objetos.push(newObj)
+
+            await fs.promises.writeFile(this.archivo, JSON.stringify(objetos, null, 2))
+            return `Nuevo id generado: ${newId}`
+
+        } catch (error) {
+            throw new Error(`Ha ocurrido un error al guardar`)       
         }
-    }).catch( err => {
-        console.log(err)
-    })
-}
+    }
 
-async function save(objeto) {
-    
-    const obj = await getAll()
+    /* 
+        getById (number) : Object
+        
+        Recibe un id y retorna el objeto con el que se relaciona ese id
+    */
+    async getById (id) {
+        try {
+            const objeto = await this.getAll()
+            return objeto.find( obj => obj.id === id )
+        } catch (error) {
+            throw new Error(`Ha ocurrido un error al lista el elemento ${id}: ${error}`)       
+        }
+    }
 
-    let newId
-    if (obj.length == 0) {
-        newId = 1
-    } else {
-        newId = obj[obj.length - 1].id + 1   
+    /* 
+        getById () : Object []
+        
+        Retorna el array de objetos del archivo
+    */
+    async getAll () {
+        try {
+            const objs = await fs.promises.readFile(this.archivo, 'utf-8')
+            return JSON.parse(objs)
+        } catch (error) {
+            return []
+        }
+    }
+
+    /* 
+        deleteById (number) : void
+        
+        Recibe un id y elimina el objeto con ese id
+    */
+    async deleteById (id) {
+        try {
+            const objeto = await this.getAll()
+            const obj = objeto.filter( i => i.id !== id)
+            fs.promises.writeFile(this.archivo, JSON.stringify(obj, null, 2))
+        } catch (error) {
+            throw new Error(`Ha ocurrido un error al eliminar el elemento ${id}: ${error}`)
+        }
+    }
+
+    /* 
+        deleteAll () : void
+        
+        Elimina todos los objetos presentes en el archivo
+    */
+    async deleteAll () {
+        try {
+            await fs.promises.writeFile(this.archivo, '')
+            return 'Contenido borrado exitosamente'
+        } catch (error) {
+            throw new Error(`Ha ocurrido un error al eliminar: ${error}`)
+        }
     }
     
-    const newObj = {...objeto, id: newId}
-    obj.push(newObj)
 
-    try {
-        await fs.writeFile(text, JSON.stringify(obj, null, 2))
-        return newId
-    } catch (error) {
-        throw new Error(`Error al guardar ${error}`)
-    }
 }
 
-async function getById(id) {
-    const objeto = await getAll()
-    return objeto.find( obj => obj.id === id )
-}
-
-async function getAll() {
-    try {
-        const objs = JSON.parse(await fs.readFile(text, 'utf-8'), null, 2)
-        return objs
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-async function deleteById(id) {
-    let objeto = await getAll()
-    let obj = objeto.filter( obj => obj.id !== id )
-    
-    try {
-        await fs.writeFile(text, JSON.stringify(obj, null, 2))
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-async function deleteAll() {
-    await fs.writeFile(text, '[]') 
-}
-
-async function main() {
-    await deleteAll()
-    console.log(await getAll())
+//Metodo lanzador
+async function main(){
+    const c = new Contenedor("./productos.txt")
+    console.log(await c.getAll())
+    console.log(await c.save({"title" : "titulo del producto", "price" : 1.99, "thumbnail" : "Url foto producto"}))
+    console.log(await c.save({"title" : "titulo del producto 2", "price" : 2.99, "thumbnail" : "Url foto producto 2 "}))
+    console.log(await c.save({"title" : "titulo del producto 3", "price" : 3.99, "thumbnail" : "Url foto producto 3"}))
+    console.log(await c.save({"title" : "titulo del producto 4", "price" : 4.99, "thumbnail" : "Url foto producto 4"}))
+    console.log(await c.save({"title" : "titulo del producto 5", "price" : 5.99, "thumbnail" : "Url foto producto 5"}))
+    console.log(await c.getAll())
+    console.log(await c.getById(2))
+    console.log(await c.getById(5))
+    console.log(await c.deleteById(2))
+    console.log(await c.deleteById(3))
+    console.log(await c.getAll())
+    console.log(await c.deleteAll())
+    console.log(await c.getAll())
 }
 main()
