f5d82976badf66b91582ea1d7cbbb1881c4efb36
Leandro Mesa <64225835+Leancen22@users.noreply.github.com>
2022-09-07 01:29:41 -0300

    Delete PrimeraEntregaFinal directory

--- PrimeraEntregaFinal/src/routes/productos.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const routerProductos = express.Router();
-
-routerProductos.get('/', (req, res) => {
-    res.status(200).json({code: 200, msg: "Test"})
-})
-
-module.exports = routerProductos;
