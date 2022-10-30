08d07c26af9ed58b4d0e7c9d1dc64e02b981607d
Leandro Mesa <leandromesaprofesional@gmail.com>
2022-10-30 19:12:37 -0300

    Entrega 14

--- Entrega14/src/routers/randoms.js
+++ Entrega14/src/routers/randoms.js
@@ -1,23 +1,23 @@
 //Retortna numeros del 1 al 1000 aleatorios
-const randomNum = () => {
+const numeroAleatorio = () => {
     return Math.floor(Math.random() * 1000 + 1);
 };
   
-const calcularNums = (cant) => {
-    const numbers = {};
+const coincidencias = (cant) => {
+    const numeros = {};
     //Se ejecuta cant veces, este parametro es enviado por fork.send
     while (cant > 0) {
-        const num = randomNum();
+        const num = numeroAleatorio();
         //Si se encuentra el numero obtenido por random suma 1 a la cantidad , sino lo define como 1
-        numbers[num] ? numbers[num] += 1 : numbers[num] = 1;
+        numeros[num] ? numeros[num] += 1 : numeros[num] = 1;
         cant -= 1;
     }
-    return numbers;
+    return numeros;
 };
   
 //Mientras el proceso continua se envian los numeros
 process.on('message', msg => {
-    const numeros = calcularNums(msg.cant);
+    const numeros = coincidencias(msg.cant);
     process.send({ numeros });
 });
 
